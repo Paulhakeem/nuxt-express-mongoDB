34f3bf7b23d1f7f8a9e076c48cfbbca9103d52a0
Paul <paulnyamawi18@example.com>
2024-08-25 13:26:42 +0300

    adding users page

--- client/components/loading.vue
+++ client/components/loading.vue
@@ -3,7 +3,7 @@
     <div role="status" class="justify-center flex m-auto pt-12">
       <svg
         aria-hidden="true"
-        class="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-[#07d884]"
+        class="w-8 h-8 text-gray-200 font-semibold animate-spin dark:text-gray-600 fill-[#07d884]"
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
--- client/components/userHome.vue
+++ client/components/userHome.vue
@@ -1,19 +1,49 @@
 <template>
-    <div class="flex flex-wrap justify-center items-center space-x-10 md:items-center">
-      <!-- user -->
-      <div class="w-96 h-96 mt-10 border border-1 border-gray-500 rounded-md">
-       chats
-      </div>
-      <div class="w-96 h-96 mt-10 border border-1 border-gray-500 rounded-md">
-      user
-      </div>
+  <div class="bg-[#f4f5f7] mt-8 p-6">
+    <div v-if="isLoading" class="text-center justify-center pt-4">
+      <loading />
     </div>
+    <div v-for="user in allUsers" :key="user.id" class="border-b-2">
+      <ul class="flex justify-between space-x-6 border-gray-300 p-2">
+        <li class="flex space-x-3">
+            <font-awesome-icon :icon="['fas', 'user']" class="text-3xl pt-2"/>
+          <div>
+            <h1 class="text-[#2c2e32] font-medium first-letter:uppercase">
+              {{ user.name }}
+            </h1>
+            <p class="text-xs text-[#8f9aac]">{{ user.email }}</p>
+          </div>
+        </li>
+        <div class="flex gap-2 pt-2">
+            <span class="text-sm text-gray-500">Joined:</span>
+            <p class="text-[#2c2e32] text-sm">4/7/2024</p>
+        </div>
+        <button
+          class="bg-gray-800 p-2 w-42 uppercase text-[#07d884] text-sm rounded-sm"
+        >
+          send message
+        </button>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script setup>
-
+const allUsers = useState("users", () => {});
+const isLoading = ref(true);
+onMounted(async () => {
+  try {
+    const data = await $fetch("/api/users");
+    const users = await data.res.data.users;
+    if (users) {
+      allUsers.value = users;
+    }
+  } catch (error) {
+    console.log(error.message);
+  } finally {
+    isLoading.value = false;
+  }
+});
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
