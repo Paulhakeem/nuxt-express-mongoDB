57165a317f850f842bee542592e9907ad29bf925
Paul <paulnyamawi18@gmail.com>
2024-09-15 21:49:00 +0300

    adding messages container

--- client/pages/messages.vue
+++ client/pages/messages.vue
@@ -1,13 +1,86 @@
 <template>
-    <div>
-hello
+  <div
+    class="flex flex-col items-center justify-center w-screen min-h-screen p-10"
+  >
+    <div
+      class="flex flex-col flex-grow w-full max-w-4xl bg-white shadow-xl rounded-lg overflow-hidden"
+    >
+      <div class="flex flex-col flex-grow h-0 p-4 overflow-auto">
+        <div class="flex w-full mt-2 space-x-3 max-w-xs">
+          <img
+            src="../assets/img/user.jpg"
+            class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300 object-cover"
+          />
+          <div>
+            <div class="bg-gray-800 p-3 rounded-r-lg rounded-bl-lg">
+              <p class="text-sm text-gray-200 text-pretty">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </p>
+            </div>
+            <span class="text-xs text-gray-500 leading-none">2 min ago</span>
+          </div>
+        </div>
+
+        <div class="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end">
+          <div>
+            <div class="bg-[#07d884] text-white p-3 rounded-l-lg rounded-br-lg">
+              <p class="text-sm text-gray-200 text-pretty">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod.
+              </p>
+            </div>
+            <span class="text-xs text-gray-500 leading-none">2 min ago</span>
+          </div>
+          <img
+            src="../assets/img/user.jpg"
+            class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300 object-cover"
+          />
+        </div>
+      </div>
+
+      <div class="flex w-full mt-2 space-x-3 max-w-xs">
+        <img
+            src="../assets/img/user.jpg"
+            class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300 object-cover"
+          />
+        <div>
+          <div class="bg-gray-800 p-3 rounded-r-lg rounded-bl-lg">
+            <p class="text-sm text-gray-200 text-pretty">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
+          <span class="text-xs text-gray-500 leading-none">2 min ago</span>
+        </div>
+      </div>
+
+      <div class="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end">
+        <div>
+          <div class="bg-[#07d884] text-white p-3 rounded-l-lg rounded-br-lg">
+            <p class="text-sm text-gray-200 text-pretty">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
+          <span class="text-xs text-gray-500 leading-none">2 min ago</span>
+        </div>
+        <img
+            src="../assets/img/user.jpg"
+            class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300 object-cover"
+          />
+      </div>
+      <div class="bg-gray-300 p-4">
+      <input
+        class="flex items-center h-10 w-full rounded px-3 text-sm"
+        type="text"
+        placeholder="Type your message…"
+      />
     </div>
+    </div>
+   
+  </div>
 </template>
 
-<script setup>
+<script setup></script>
 
-</script>
-
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
