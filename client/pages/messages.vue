c2470c5fc84d83c53cc1b704ddcfe7afcce65970
Paul <paulnyamawi18@gmail.com>
2024-09-16 23:23:16 +0300

    styling tommorow

--- client/pages/messages.vue
+++ client/pages/messages.vue
@@ -170,45 +170,6 @@
                             </div>
                           </div>
                         </div>
-                        <div class="col-start-1 col-end-8 p-3 rounded-lg">
-                          <div class="flex flex-row items-center">
-                            <div
-                              class="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0"
-                            >
-                              A
-                            </div>
-                            <div
-                              class="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl"
-                            >
-                              <div class="flex flex-row items-center">
-                                <button
-                                  class="flex items-center justify-center bg-indigo-600 hover:bg-indigo-800 rounded-full h-8 w-10"
-                                >
-                                  <svg
-                                    class="w-6 h-6 text-white"
-                                    fill="none"
-                                    stroke="currentColor"
-                                    viewBox="0 0 24 24"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                  >
-                                    <path
-                                      stroke-linecap="round"
-                                      stroke-linejoin="round"
-                                      stroke-width="1.5"
-                                      d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
-                                    ></path>
-                                    <path
-                                      stroke-linecap="round"
-                                      stroke-linejoin="round"
-                                      stroke-width="1.5"
-                                      d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                                    ></path>
-                                  </svg>
-                                </button>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
                       </div>
                     </div>
                   </div>
@@ -219,68 +180,29 @@
                       <button
                         class="flex items-center justify-center text-gray-400 hover:text-gray-600"
                       >
-                        <svg
-                          class="w-5 h-5"
-                          fill="none"
-                          stroke="currentColor"
-                          viewBox="0 0 24 24"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"
-                          ></path>
-                        </svg>
+                      <font-awesome-icon :icon="['fas', 'paperclip']" />
                       </button>
                     </div>
                     <div class="flex-grow ml-4">
                       <div class="relative w-full">
                         <input
                           type="text"
-                          class="flex w-full border rounded-xl focus:outline-none focus:border-indigo-300 pl-4 h-10"
+                          class="flex w-full border rounded-xl focus:outline-none focus:border-[#07d884] pl-4 h-10"
                         />
                         <button
                           class="absolute flex items-center justify-center h-full w-12 right-0 top-0 text-gray-400 hover:text-gray-600"
                         >
-                          <svg
-                            class="w-6 h-6"
-                            fill="none"
-                            stroke="currentColor"
-                            viewBox="0 0 24 24"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                              stroke-width="2"
-                              d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                            ></path>
-                          </svg>
+                        <font-awesome-icon :icon="['fas', 'link']" />
                         </button>
                       </div>
                     </div>
                     <div class="ml-4">
                       <button
-                        class="flex items-center justify-center bg-indigo-500 hover:bg-indigo-600 rounded-xl text-white px-4 py-1 flex-shrink-0"
+                        class="flex items-center justify-center bg-gray-800 hover:bg-gray-600 rounded-xl text-[#07d884] px-4 py-1 flex-shrink-0"
                       >
                         <span>Send</span>
                         <span class="ml-2">
-                          <svg
-                            class="w-4 h-4 transform rotate-45 -mt-px"
-                            fill="none"
-                            stroke="currentColor"
-                            viewBox="0 0 24 24"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                              stroke-width="2"
-                              d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
-                            ></path>
-                          </svg>
+                          <font-awesome-icon :icon="['fas', 'paper-plane']" />
                         </span>
                       </button>
                     </div>
@@ -290,6 +212,7 @@
             </div>
           </div>
         </div>
+        <!--  -->
       </div>
     </div>
   </main>
