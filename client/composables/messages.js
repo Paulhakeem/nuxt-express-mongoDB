05abc8cc924bafa4735678491b70e29bb6994a4c
Paul <paulnyamawi18@gmail.com>
2024-09-18 20:12:36 +0300

    add all needed functions

---
+++ client/composables/messages.js
@@ -0,0 +1,44 @@
+export default function () {
+  // send message
+  const createMessage = async () => {
+    try {
+      const data = await $fetch("http://localhost:5000/api/message", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: { text },
+      });
+
+      if (data) {
+        console.log(data);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  //   get Messages
+  const showMessages = async () => {
+    try {
+      const data = await $fetch("http://localhost:5000/api/messages");
+      if (data) {
+        console.log(data);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+//   delete Message
+const deleteMessages = async () => {
+    try {
+      const data = await $fetch(`http://localhost:5000/api/message/${id}`);
+      if (data) {
+        console.log('message deleted');
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  return { createMessage, showMessages, deleteMessages };
+}
